174e811b9fba8a02130807f48bdde90740aaf65f
codedlovely <submerginglife@gmail.com>
2022-04-29 16:42:27 +0900

    Modify layout for mobile devices

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # hamakaze-bakery
 A landing page for a fictional local bakery.
+
+### Photo credit
+[Photo by Tran from Pexels](https://www.pexels.com/photo/macro-shot-photography-of-bread-near-chef-564883/)
+
+[Photo by Silvia Trigo from Pexels](https://www.pexels.com/photo/cakes-on-table-1857157/)
+
+[Photo by Ela Haney from Pexels](https://www.pexels.com/photo/close-up-photography-of-chocolate-cupcake-90609/)
+
+[Photo by Polina Tankilevitch from Pexels](https://www.pexels.com/photo/photo-of-chocolate-cake-on-wooden-plate-4109998/)
+
+[Photo by Laura James from Pexels](https://www.pexels.com/photo/traditional-christmas-cookies-in-box-6102134/)
+
+### JavaScript references
+[webdev.tech](https://web-dev.tech/front-end/javascript/accordion/)
+[w3schools.com](https://www.w3schools.com/howto/howto_js_scroll_to_top.asp)
--- javascript.js
+++ javascript.js
@@ -91,3 +91,37 @@ const slideUp = (el, duration = 300) => {
       });
     });
   });
+
+  // Scroll back to top
+mybutton = document.getElementById("scrollBack");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// display hamburger menu
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+// close hamburger menu
+const navLink = document.querySelectorAll(".navilink");
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
