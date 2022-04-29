e5f33320aa81a74e8883d02d911de3eddef3982d
codedlovely <submerginglife@gmail.com>
2022-04-29 11:29:17 +0900

    Add parallax background

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
-// accordion faq section
+// expand or contract FAQ item
+const curFaq = document.querySelectorAll('.question');
+function toggle() {
+    const curAnswer = this.nextElementSibling;
+    this.classList.toggle('active');
+    curAnswer.classList.toggle('open');
+}
+for (let i=0; i<curFaq.length; i++) {
+    curFaq[i].addEventListener('click', toggle);
+}
